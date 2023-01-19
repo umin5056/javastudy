aca7187bcab6b07142929c31938718e91a95643d
Jowoomin <umin5056@naver.com>
2023-01-19 19:34:14 +0900

    23-01-19

---
+++ Exam02/src/parking/MainClass.java
@@ -0,0 +1,15 @@
+package parking;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		ParkingLot pl = new ParkingLot("조우민");
+		
+		pl.addCar();
+		pl.addCar();
+		pl.addCar();
+		pl.addCar();
+	}
+
+}
---
+++ Exam02/src/parking/ParkingLot.java
@@ -0,0 +1,73 @@
+package parking;
+
+import java.util.Scanner;
+
+public class ParkingLot {
+
+	private String name;
+	private Car[] cars;
+	private int idx;
+	private Scanner sc;
+
+	public ParkingLot (String name) {
+		this.name = name;
+		cars = new Car[10];
+		sc = new Scanner(System.in);
+	}
+
+	public void addCar() {
+		System.out.println("현재 등록된 차량 : " + idx + "대");
+
+		if(idx == cars.length) {
+			System.out.println("더 이상 차량 등록이 불가능합니다.");
+			return;
+		}
+
+		System.out.print("차량 번호를 입력해주세요 >>> ");
+		String carNo = sc.next();
+		System.out.print("차량 모델을 입력해주세요 >>> ");
+		String model = sc.next();
+
+		cars[idx++] = new Car(carNo, model);
+		/*  방법 2
+			System.out.print("차량 번호와 모델을 입력해주세요 >>> ");
+		  	cars[idx] = new Car(sc.next(), sc.next());
+		 */
+
+		System.out.println("차량번호 " + carNo + " 차량이 등록되었습니다.");
+
+	}
+
+	public void deleteCar() {
+		
+	}
+
+	public void printAllCar() {
+		
+	}
+	
+	public void manage() {
+		while(true) {
+			
+			System.out.println("1.추가 2.삭제 3.전체 0.종료");
+			
+			int choice = sc.nextInt();
+			
+			switch(choice) {
+			
+			case 1: addCar(); break;
+			
+			case 2:	deleteCar(); break;
+			
+			case 3: printAllCar(); break;
+				
+			case 0:	System.out.println("프로그램 종료"); return;
+			
+			default : System.out.println("잘못 입력하셨습니다.");
+			}
+			
+			
+		}
+	}
+	
+}
