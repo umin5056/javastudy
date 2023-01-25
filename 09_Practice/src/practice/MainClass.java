f73a6e436ea46bb940f8adc8b5a4e63e1104bacb
Jowoomin <umin5056@naver.com>
2023-01-25 17:39:43 +0900

    23-01-25 END

--- 09_Practice/src/practice/MainClass.java
+++ 09_Practice/src/practice/MainClass.java
@@ -1,8 +1,8 @@
 package practice;
 
 import java.io.File;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Calendar;
 import java.util.Date;
@@ -70,58 +70,105 @@ public class MainClass {
 	 */
 
 	public static void ex02() {
+//		조우
+//		File dir = new File("/Library/Java/JavaVirtualMachines/jdk-11.0.17.jdk/Contents/Home");
+//		File[] files = dir.listFiles();
+//
+//		Date now = new Date();
+//		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd  a hh:mm");
+//
+//		long totalSize = 0;
+//		int count = 0;
+//
+//		for(int i=0; i<files.length; i++) {
+//			if(files[i].isHidden() == false) {
+//		String time = sdf.format(files[i].lastModified());
+//
+//				if(files[i].isDirectory()) {
+//					System.out.println(time + "\t" + "<DIR>" + "\t\t" + files[i].getName());
+//
+//				} else {
+//
+//					System.out.println(time + "\t" + "\t" + files[i].length() +"\t"+ files[i].getName());
+//					totalSize += files[i].length();
+//					count++;
+//
+//				}
+//			}
+//		}
+//		System.out.println("\t\t" +count+"개 파일" +"\t\t" + totalSize +"\t" +"바이트");
+		
+		
+		File dir = new File("/Library/Java/JavaVirtualMachines/jdk-11.0.17.jdk/Conten"
+				+ "ts/Home");
 
-		File dir = new File("/Library/Java/JavaVirtualMachines/jdk-11.0.17.jdk/Contents/Home");
-		File[] files = dir.listFiles();
+		if(dir.exists()) {
+			File[] list = dir.listFiles();
+			
+			int fileCount = 0;
+			long totalFileSize = 0;
 
-		Date now = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd  a hh:mm");
-
-		String time = sdf.format(now);
-		long totalSize = 0;
-		int count = 0;
-
-		for(int i=0; i<files.length; i++) {
-			if(files[i].isHidden() == false) {
-
-				if(files[i].isDirectory()) {
-					System.out.println(time + "\t" + "<DIR>" + "\t\t" + files[i].getName());
-				} else {
-					System.out.println(time + "\t" + "\t" + files[i].length() +"\t"+ files[i].getName());
-					totalSize += files[i].length();
-					count++;
+			for(File f : list) {
+				if(f.isHidden()) {
+					continue;
 				}
+				String lastModifiedDate = new SimpleDateFormat("yyyy-MM-dd  a hh:mm").format(f.lastModified());
+				String directory = f.isDirectory() ? "<DIR>" : "";
+				String size = ""; 
+						if(f.isFile()){
+							size = new DecimalFormat("#,##0").format(f.length()); 
+							fileCount++;
+							totalFileSize += f.length();
+						}
+				String name = f.getName();
+				String result = String.format("%s%9s%9s %s\n", lastModifiedDate, directory, size, name);
+				System.out.print(result);
 			}
+				System.out.println(fileCount + "개 파일 " + new DecimalFormat("#,##0").format(totalFileSize) + " 바이트");
 		}
-		System.out.println("\t\t" +count+"개 파일" +"\t\t" + totalSize +"\t" +"바이트");
+
 	}
 
 	// 문제3. Documents에 저장된 storage를 삭제하시오.
 	//		 파일이 저장된 디렉터리를 지워지지 않으므르 먼저 디렉터리에 저장된 파일을 삭제해야 한다.
 
 	public static void ex03() {
-
+// 조우민
+//		File dir = new File("/Users/woomin/Documents/storage");
+//
+//		if(dir.exists()) {
+//			File[] files = dir.listFiles();
+//
+//			for(File f : files) {
+//				f.delete();
+//			}
+//
+//			dir.delete();
+//
+//			if(dir.exists()) {
+//				System.out.println("삭제 실패!");
+//			} else {
+//				System.out.println("삭제 성공!");
+//			}
+//		}
+	
 		File dir = new File("/Users/woomin/Documents/storage");
-
-
-		if(dir.exists()) {
-			File[] files = dir.listFiles();
-
-			for(File f : files) {
-				f.delete();
-			}
-
-			dir.delete();
-
-			if(dir.exists()) {
-				System.out.println("삭제 실패!");
-			} else {
-				System.out.println("삭제 성공!");
-			}
+		
+		File file = new File(dir, "myfile.txt");
+		
+		if(file.exists()) {
+			file.delete();
 		}
+		
+		if(dir.exists()) {
+			dir.delete();
+		}
+		
+		
+	
 	}
 
-	public static void main(String[] args){ // main 메소드를 호출한 곳으로 예외처리를 넘긴다. (개발자가 try-catch 하지 않겠다.)
+	public static void main(String[] args){ 
 		ex02();
 	}
 
